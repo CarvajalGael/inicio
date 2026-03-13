ceb04bcbac38cedb6a10f03b8293c0caa94384e1
CarvajalGael <23308060610312@cetis61.edu.mx>
2026-03-13 09:56:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import flet as ft
 def main(page: ft.Page): 
     page.title = "INICIO DE SESION"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER 
+    
+    
     
     titulo = ft.Text(
         value="INICIAR SESION", 
@@ -11,19 +13,33 @@ def main(page: ft.Page):
         weight=ft.FontWeight.BOLD
     )
     
+    nombre = ft.TextField(
+        label="Nombre de Usuario", 
+        hint_text="Juana", 
+        width=300,
+    )
+    
     correo = ft.TextField(
         label="Correo electrónico", 
         hint_text="ejemplo@dominio.com", 
         width=300,
-        icon=ft.Icons.EMAIL 
-        )
+        icon=ft.Icons.EMAIL
+    )
     
     password = ft.TextField(
         label="Contraseña", 
         width=300,
-        icon=ft.Icons.PASSWORD, 
-        )  
-    
-    page.add(titulo,correo, password )
+        icon=ft.Icons.PASSWORD,
+        password=True
+    )
+    boton_login = ft.Button(content="Iniciar Sesion")
+
+    boton_texto = ft.TextButton(
+        content="¿Olvidaste tu contraseña?",
+        icon=ft.Icons.STAR_BORDER,
+        icon_color=ft.Colors.BLUE_300,
+    )
+
+    page.add(titulo, nombre, correo, password, boton_login, boton_texto)
 
 ft.app(target=main)
