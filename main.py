a3fb3bc5f00d26621ae57e98c12f4eae476b58d1
CarvajalGael <23308060610312@cetis61.edu.mx>
2026-03-13 19:00:58 -0600

    Implement login functionality and UI updates
    Added user authentication logic and updated UI components.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import flet as ft
 def main(page: ft.Page): 
     page.title = "INICIO DE SESION"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.MainAxisAlignment.CENTER 
-    
-    
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER 
+
+    USUARIO_CORRECTO = "admin"
+    CORREO_CORRECTO = "admin@dominio.com"
+    CONTRA_CORRECTA = "1234"
     
     titulo = ft.Text(
         value="INICIAR SESION", 
@@ -17,6 +19,7 @@ def main(page: ft.Page):
         label="Nombre de Usuario", 
         hint_text="Juana", 
         width=300,
+        icon=ft.Icons.PERSON
     )
     
     correo = ft.TextField(
@@ -32,14 +35,54 @@ def main(page: ft.Page):
         icon=ft.Icons.PASSWORD,
         password=True
     )
-    boton_login = ft.Button(content="Iniciar Sesion")
+
+    def iniciar_sesion(e):
+        if nombre.value == USUARIO_CORRECTO and correo.value == CORREO_CORRECTO and password.value == CONTRA_CORRECTA:
+            
+            page.clean()
+
+            barra = ft.NavigationBar(
+                destinations=[
+                    ft.NavigationBarDestination(icon=ft.Icons.HOME, label="HOME"),
+                    ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="EXPLORAR"),
+                    ft.NavigationBarDestination(icon=ft.Icons.PERSON, label="PERFIL"),
+                ]
+            )
+
+            page.navigation_bar = barra
+
+            titulo_panel = ft.Text(
+                "Panel Principal",
+                size=40,
+                weight=ft.FontWeight.BOLD
+            )
+
+            bienvenida = ft.Text(
+                "Bienvenido al sistema",
+                size=20
+            )
+
+            page.add(titulo_panel, bienvenida)
+
+        else:
+            page.snack_bar = ft.SnackBar(
+                ft.Text("Usuario, correo o contraseña incorrectos")
+            )
+            page.snack_bar.open = True
+
+        page.update()
+
+    boton_login = ft.ElevatedButton(
+        "Iniciar Sesion",
+        on_click=iniciar_sesion
+    )
 
     boton_texto = ft.TextButton(
         content="¿Olvidaste tu contraseña?",
         icon=ft.Icons.STAR_BORDER,
         icon_color=ft.Colors.BLUE_300,
     )
-
+    
     page.add(titulo, nombre, correo, password, boton_login, boton_texto)
 
 ft.app(target=main)
